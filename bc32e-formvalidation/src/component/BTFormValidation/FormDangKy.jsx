7d15b87422c844cd1e5bacb121c748b4b0f17445
ducthecyber <phuongduducthe@gmail.com>
2022-09-20 01:03:13 +0700

    formValidation

--- bc32e-formvalidation/src/component/BTFormValidation/FormDangKy.jsx
+++ bc32e-formvalidation/src/component/BTFormValidation/FormDangKy.jsx
@@ -15,7 +15,7 @@ class FormDangKy extends Component {
             phoneNumber: '',
             email: '',
         },
-        disabled: true,
+        disabled: false,
 
     }
 
@@ -30,50 +30,43 @@ class FormDangKy extends Component {
         }
 
         //kiểm tra mã SV
-        if(event.target.name === 'maSV'){
+        if (event.target.name === 'maSV') {
             let regexNumber = /^[0-9]+$/;
-            if(!regexNumber.test(value)){
+            if (!regexNumber.test(value)) {
                 errorMessage = name + ' must be numbers !'
             }
         }
 
         //kiểm tra tên
-        if(event.target.name === 'hoTen'){
-            let regexLetter = /^[A-Z a-z]+$/;   
-            if(!regexLetter.test(value)){
+        if (event.target.name === 'hoTen') {
+            let regexLetter = /^[A-Z a-z]+$/;
+            if (!regexLetter.test(value)) {
                 errorMessage = name + ' must be letters !'
             }
         }
 
         //kiểm tra email
-        if (event.target.type === 'email' ) {
+        if (event.target.type === 'email') {
             let regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
             if (!regexEmail.test(value)) {
-                errorMessage = name + ' should be example@gmail.com !';
+                errorMessage = name + ' should be example@mail.com !';
             }
         }
 
-        if(event.target.type === 'number'){
+        if (event.target.type === 'number') {
             let regexPhoneNumber = /(84|0[3|5|7|8|9])+([0-9]{8})\b/;
-            if(!regexPhoneNumber.test(value)){
+            if (!regexPhoneNumber.test(value)) {
                 errorMessage = name + ' starts with 03, 05, 07, 09 or 84! and have 10 numbers'
             }
         }
 
-        if(errorMessage === ''){
-            this.setState({
-                disabled:false,
-            })
-        }
-
         let values = { ...this.state.value, [name]: value };
         let errors = { ...this.state.error, [name]: errorMessage }
+        
         this.setState({
             value: values,
             error: errors,
         })
-        console.log(value)
-        // console.log(this.state)
     }
 
     render() {
@@ -111,9 +104,9 @@ class FormDangKy extends Component {
                     </div>
                 </div>
                 <div className='text-left'>
-                    <button disabled={this.state.disabled} type='submit' className='bg-green-500 p-3 mt-4 text-white rounded-md ml-0'>Thêm Sinh Viên</button>
-                </div>
-            </form>
+                    <button disabled={this.state.disabled} type='submit' className='bg-green-700 hover:bg-green-500 p-3 mt-4 text-white rounded-md ml-0'>Thêm Sinh Viên</button>
+            </div>
+            </form >
         )
     }
 }
--- bc32e-formvalidation/src/store/reducers/sinhVien.js
+++ bc32e-formvalidation/src/store/reducers/sinhVien.js
@@ -1,23 +1,38 @@
 const stateDefault = {
-    mangSinhVien:[
+    mangSinhVien: [
         {
-            maSV:'1',
-            hoTen:'microsoft',
-            phoneNumber:'0909123456',
-            email:'microsoft@gmail.com',
+            maSV: '1',
+            hoTen: 'microsoft',
+            phoneNumber: '0909123456',
+            email: 'microsoft@gmail.com',
         }
     ],
 }
 
 export const infoSinhVien = (state = stateDefault, action) => {
     switch (action.type) {
-      case 'ADD_SINH_VIEN':{
-        console.log(action)
-        const mangSVUpdate = [...state.mangSinhVien,action.payload.value];
-        state.mangSinhVien = mangSVUpdate
-        return {...state}
-      };
+        case 'ADD_SINH_VIEN': {
+            
+            let valid = false;
+            const arrError = action.payload.error
+            for (let key in arrError) {
+                if (arrError[key] === '') {
+                    valid = true;
+                }
+            }
+            const arrValue = action.payload.value
+            for (let key in arrValue){
+                if(arrValue[key] === ''){
+                    valid = false;
+                }
+            }
+            if (valid) {
+                const mangSVUpdate = [...state.mangSinhVien, action.payload.value];
+                state.mangSinhVien = mangSVUpdate
+                return { ...state }
+            }
+        };
 
-        default:return {...state}
+        default: return { ...state }
     }
 }
